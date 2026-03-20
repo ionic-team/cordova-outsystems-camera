5eb59698bd5122275035b48be3d62b860df0be3d
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-03-20 13:06:44 +0000

    chore(outsystems-wrapper): Format

--- packages/outsystems-wrapper/src/index.ts
+++ packages/outsystems-wrapper/src/index.ts
@@ -1,11 +1,11 @@
 import {
-  GalleryOptions,
-  MediaResult,
-  EditURIPhotoOptions,
-  PlayVideoOptions,
-  PluginError,
-  RecordVideoOptions,
-  TakePhotoOptions
+    GalleryOptions,
+    MediaResult,
+    EditURIPhotoOptions,
+    PlayVideoOptions,
+    PluginError,
+    RecordVideoOptions,
+    TakePhotoOptions
 } from "../../cordova-plugin/src/definitions";
 import { checkIfPWA, isUnifiedPluginDefined, isCapacitorPluginDefined } from "./helpers";
 
@@ -21,130 +21,130 @@ import { checkIfPWA, isUnifiedPluginDefined, isCapacitorPluginDefined } from "./
 
 class OSCameraPlugin {
 
-  takePhoto(
-    success: (result: MediaResult) => void,
-    error: (err: PluginError) => void,
-    options: TakePhotoOptions
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
+    takePhoto(
+        success: (result: MediaResult) => void,
+        error: (err: PluginError) => void,
+        options: TakePhotoOptions
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
+
+        if (isUnifiedPluginDefined()) {
+            let directionInteger: any = options.cameraDirection as any;
+            if (directionInteger == 1) {
+                options.cameraDirection = 'FRONT';
+            } else {
+                options.cameraDirection = 'REAR';
+            }
+            if (isCapacitorPluginDefined()) {
+                
+            } else {
+
+            }
+        } else {
+            let correctedLegacyOptions: any = options;
+            correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
+            // @ts-ignore
+            if (typeof (Camera) !== "undefined") {
+                // @ts-ignore
+                correctedLegacyOptions.destinationType = Camera.DestinationType.DATA_URL;
+                // @ts-ignore
+                correctedLegacyOptions.source = Camera.PictureSourceType.CAMERA;
+            }
+            // @ts-ignore
+            navigator.camera.takePicture(success, error, correctedLegacyOptions);
+        }
     }
 
-    if (isUnifiedPluginDefined()) {
-      let directionInteger: any = options.cameraDirection as any;
-      if (directionInteger == 1) {
-        options.cameraDirection = 'FRONT';
-      } else {
-        options.cameraDirection = 'REAR';
-      }
-      if (isCapacitorPluginDefined()) {
-        
-      } else {
-        
-      }
-    } else {
-      let correctedLegacyOptions: any = options;
-      correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
-      // @ts-ignore
-      if (typeof(Camera) !== "undefined") {
-        // @ts-ignore
-        correctedLegacyOptions.destinationType = Camera.DestinationType.DATA_URL;
-        // @ts-ignore
-        correctedLegacyOptions.source = Camera.PictureSourceType.CAMERA;
-      }
-      // @ts-ignore
-      navigator.camera.takePicture(success, error, correctedLegacyOptions);
-    }
-  }
+    chooseFromGallery(
+        success: (result: MediaResult[]) => void,
+        error: (err: PluginError) => void,
+        options: GalleryOptions
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
 
-  chooseFromGallery(
-    success: (result: MediaResult[]) => void,
-    error: (err: PluginError) => void,
-    options: GalleryOptions
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
+        if (isUnifiedPluginDefined()) {
+            // TODO call unified wrapper
+        } else {
+            // @ts-ignore
+            navigator.camera.chooseFromGallery(success, error, options);
+        }
     }
 
-    if (isUnifiedPluginDefined()) {
-      // TODO call unified wrapper
-    } else {
-      // @ts-ignore
-      navigator.camera.chooseFromGallery(success, error, options);
-    }
-  }
+    editPhoto(
+        success: (imageData: any) => void,
+        error: (err: PluginError) => void,
+        input: { image: string }
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
 
-  editPhoto(
-    success: (imageData: any) => void,
-    error: (err: PluginError) => void,
-    input: {image: string}
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
-    }
-
-    if (isUnifiedPluginDefined()) {
-      // TODO call unified wrapper
-    } else {
-      // @ts-ignore
-      navigator.camera.editPicture(success, error, input);
-    }
-  }
-
-  editURIPhoto(
-    success: (result: MediaResult) => void,
-    error: (err: PluginError) => void,
-    options: EditURIPhotoOptions
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
+        if (isUnifiedPluginDefined()) {
+            // TODO call unified wrapper
+        } else {
+            // @ts-ignore
+            navigator.camera.editPicture(success, error, input);
+        }
     }
 
-    if (isUnifiedPluginDefined()) {
-      // TODO call unified wrapper
-    } else {
-      let correctedLegacyOptions: any = options;
-      correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
-      // @ts-ignore
-      navigator.camera.editURIPicture(success, error, correctedLegacyOptions);
-    }
-  }
+    editURIPhoto(
+        success: (result: MediaResult) => void,
+        error: (err: PluginError) => void,
+        options: EditURIPhotoOptions
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
 
-  recordVideo(
-    success: (result: MediaResult) => void,
-    error: (err: PluginError) => void,
-    options: RecordVideoOptions
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
-    }
-
-    if (isUnifiedPluginDefined()) {
-      // TODO call unified wrapper
-    } else {
-      let correctedLegacyOptions: any = options;
-      correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
-      // @ts-ignore
-      navigator.camera.recordVideo(success, error, correctedLegacyOptions);
-    }
-  }
-
-  playVideo(
-    success: () => void,
-    error: (err: PluginError) => void,
-    options: PlayVideoOptions
-  ): void {
-    if (checkIfPWA(error)) {
-      return;  // PWA implementation is outside this wrapper's scope
+        if (isUnifiedPluginDefined()) {
+            // TODO call unified wrapper
+        } else {
+            let correctedLegacyOptions: any = options;
+            correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
+            // @ts-ignore
+            navigator.camera.editURIPicture(success, error, correctedLegacyOptions);
+        }
     }
 
-    if (isUnifiedPluginDefined()) {
-      // TODO call unified wrapper
-    } else {
-      // @ts-ignore
-      navigator.camera.playVideo(success, error, options);
+    recordVideo(
+        success: (result: MediaResult) => void,
+        error: (err: PluginError) => void,
+        options: RecordVideoOptions
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
+
+        if (isUnifiedPluginDefined()) {
+            // TODO call unified wrapper
+        } else {
+            let correctedLegacyOptions: any = options;
+            correctedLegacyOptions.saveToPhotoAlbum = options.saveToGallery;
+            // @ts-ignore
+            navigator.camera.recordVideo(success, error, correctedLegacyOptions);
+        }
     }
-  }
+
+    playVideo(
+        success: () => void,
+        error: (err: PluginError) => void,
+        options: PlayVideoOptions
+    ): void {
+        if (checkIfPWA(error)) {
+            return;  // PWA implementation is outside this wrapper's scope
+        }
+
+        if (isUnifiedPluginDefined()) {
+            // TODO call unified wrapper
+        } else {
+            // @ts-ignore
+            navigator.camera.playVideo(success, error, options);
+        }
+    }
 }
 
 export const Instance = new OSCameraPlugin();
