5906723504432c519b3c262f124df4bac0d0474a
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-03-23 17:34:57 +0000

    chore(android): plugin.xml fixes and refactor namespace

--- packages/cordova-plugin/android/OSCameraPlugin.kt
+++ packages/cordova-plugin/android/OSCameraPlugin.kt
@@ -1,22 +1,4 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
-*/
-package org.apache.cordova.camera
+package com.outsystems.plugins.camera
 
 import android.Manifest
 import android.app.Activity
@@ -24,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -59,14 +40,13 @@ import org.apache.cordova.PluginResult
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
-import java.io.IOException
 
 /**
  * This class launches the camera view, allows the user to take a picture, closes the camera view,
  * and returns the captured image.  When the camera view is closed, the screen displayed before
  * the camera view was shown is redisplayed.
  */
-class CameraLauncher : CordovaPlugin() {
+class OSCameraPlugin : CordovaPlugin() {
     private var mQuality // Compression quality hint (0-100: 0=low quality & high compression, 100=compress of max quality)
             = 0
     private var targetWidth // desired width of the image
@@ -902,7 +882,7 @@ class CameraLauncher : CordovaPlugin() {
         private const val CAPTURE_VIDEO_SEC = 2
         private const val EDIT_PICTURE_SEC = 3
 
-        private const val LOG_TAG = "CameraLauncher"
+        private const val LOG_TAG = "OSCameraLib"
 
         //for errors
         private const val ERROR_FORMAT_PREFIX = "OS-PLUG-CAMR-"
